9159d167ee0b5a52654cd2c7553f0a9875f6e814
Vilzz <160743963+Vilamuzz@users.noreply.github.com>
2024-11-02 14:00:52 +0700

    another test

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -5,15 +5,11 @@ import { defineProps } from 'vue'
 defineProps({
   title: {
     type: String,
-    required: true,
+    required: false,
   },
   description: {
     type: String,
-    required: true,
-  },
-  imageSrc: {
-    type: String,
-    required: true,
+    required: false,
   },
 })
 </script>
@@ -22,11 +18,7 @@ defineProps({
     to="/category"
     class="border border-gray-300 p-7 text-left hover:bg-blue-500 hover:text-white transition duration-300"
   >
-    <img
-      :src="imageSrc"
-      alt=""
-      class="w-10 h-auto mb-2 bg-gray-100 border rounded-lg"
-    />
+    <slot></slot>
     <h3 class="text-2xl font-semibold mb-1">{{ title }}</h3>
     <p class="text-sm">{{ description }}</p>
   </router-link>
